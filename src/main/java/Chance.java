b5d7d9cae369e5768638dd0559d181494f3c54ed
Hero <heroemenalom73@gmail.com>
2023-07-15 19:27:38 -0400

    removed png file

---
+++ src/main/java/Chance.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class Chance {
+
+    private LinkedList<Integer> percentageList = new LinkedList<Integer>();
+
+    private LinkedList<String> percentageListStr = new LinkedList<String>();
+
+    public Chance(){
+
+    }
+    public Chance(int percentage){
+
+        for(int i = 0; i < percentage; i++){
+            percentageList.add(1);
+        }
+        for(int i = 0; i < 100 - percentage; i++){
+            percentageList.add(0);
+        }
+    }
+
+    public String rollStr(HashMap<String,Integer> percentages,ArrayList<String> jobsInclude){
+        System.out.println(jobsInclude);
+        for(String k : percentages.keySet()){
+            if(jobsInclude.contains(k)){
+                for(int i=0;i<percentages.get(k);i++){
+                    percentageListStr.add(k);
+                }
+            }
+        }
+
+        int randIdx = (int) (Math.random() * percentages.size());
+
+        return percentageListStr.get(randIdx);
+    }
+
+    public boolean roll(){
+        int randIdx = (int) (Math.random() * 100);
+
+        if(percentageList.get(randIdx) == 1){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+
+}
